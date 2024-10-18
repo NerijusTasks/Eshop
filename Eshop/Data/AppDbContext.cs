0cedcbf3230a5ffb1769d69c2600274df68459bc
Nerijus <Nerijus@DESKTOP-08FUU9C>
2024-10-18 22:22:18 +0300

    Add models, create migration, update database

---
+++ Eshop/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Eshop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eshop.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+                
+        }
+
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    }
+}
--- Eshop/Program.cs
+++ Eshop/Program.cs
@@ -1,4 +1,7 @@
 
+using Eshop.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Eshop
 {
     public class Program
@@ -6,6 +9,10 @@ namespace Eshop
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(
+                options => options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
 
             // Add services to the container.
 
